8343c292ac11035508b372a6b1bc4e33147ccaec
Abdul Samad M J <samadmj4@gmail.com>
2024-08-08 16:02:38 +0400

    meta tags updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,45 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>AI Steel Weight Calculator</title>
+        <link rel="icon" href="favicon.ico" sizes="any" />
+        <meta name="title" content="AI Steel Weight Calculator" />
+        <meta
+          name="description"
+          content="AI powered steel weight calculator by Metal Zone General Trading LLC"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://calculator.metalzoneuae.com/"
+        />
+        <meta property="og:title" content="AI Steel Weight Calculator" />
+        <meta
+          property="og:description"
+          content="AI powered steel weight calculator by Metal Zone General Trading LLC"
+        />
+        <meta
+          property="og:image"
+          content="https://calculator.metalzoneuae.com/ss.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://calculator.metalzoneuae.com/"
+        />
+        <meta property="twitter:title" content="AI Steel Weight Calculator" />
+        <meta
+          property="twitter:description"
+          content="AI powered steel weight calculator by Metal Zone General Trading LLC"
+        />
+        <meta
+          property="twitter:image"
+          content="https://calculator.metalzoneuae.com/ss.png"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
