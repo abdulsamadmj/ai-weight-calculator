ca186c46b770ac724f58a978d41146c830b67533
Abdul Samad M J <samadmj4@gmail.com>
2024-08-12 16:58:59 +0400

    theme switching added, rough UI updated

--- src/components/LogoComponent.tsx
+++ src/components/LogoComponent.tsx
@@ -1,13 +1,19 @@
+"use client"
+
 import Image from "next/image";
 import React from "react";
+import { useTheme } from "next-themes";
 import logoWhite from "@/assets/images/logo-white.png";
+import logoBlack from "@/assets/images/logo-black.png";
 
 function LogoComponent() {
+  const { theme } = useTheme();
+
   return (
     <div>
       <Image
-        className="w-28"
-        src={logoWhite}
+        className="w-28 h-20 object-contain"
+        src={theme === "dark" ? logoWhite : logoBlack}
         alt="Metal Zone General Trading LLC Logo"
       />
     </div>
