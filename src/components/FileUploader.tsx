86aecdd3d33c1b0796f0c4e39429e32dac962af2
Abdul Samad M J <samadmj4@gmail.com>
2024-08-17 11:10:53 +0400

    condition based rendering added to fileUploader component

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -1,6 +1,6 @@
 // src/components/FileUploader.tsx
 "use client";
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import axios from "axios";
 import { useDropzone } from "react-dropzone";
 import { PlaceholdersAndVanishInput } from "./ui/vanish-input";
@@ -15,13 +15,14 @@ import {
   IconX,
 } from "@tabler/icons-react";
 import Link from "next/link";
-import { Button, Tooltip } from "flowbite-react";
+import { Tooltip } from "flowbite-react";
 
 const FileUploader: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const [processedFileUrl, setProcessedFileUrl] = useState<string | null>(null);
+  const [textVal, setTextVal] = useState<string | null>(null);
 
   const onDrop = (acceptedFiles: File[]) => {
     if (acceptedFiles && acceptedFiles.length > 0) {
@@ -83,7 +84,10 @@ const FileUploader: React.FC = () => {
     "Calculate the weight of your Steel Products",
   ];
 
-  const textfieldChangeHandler = () => {};
+  const textfieldChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value.trim();
+    setTextVal(val.length > 0 ? val : null);
+  };
 
   const textfieldSubmitHandler = () => {};
 
@@ -139,103 +143,116 @@ const FileUploader: React.FC = () => {
         </div>
       ) : (
         <>
-          <PlaceholdersAndVanishInput
-            placeholders={placeholders}
-            onChange={textfieldChangeHandler}
-            onSubmit={textfieldSubmitHandler}
-          />
-          <div className="p-5 pb-0 text-xl text-center">OR</div>
-          <Link href={""} className="w-full flex gap-1 justify-end pr-2 pb-2">
-            Sample Data <IconInfoCircleFilled />
-          </Link>
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <BackgroundGradient className="rounded-[20px] max-w-md w-full bg-white dark:bg-zinc-900">
-              <div
-                {...getRootProps({ className: "dropzone" })}
-                className="relative flex items-center justify-center w-full rounded-3xl"
+          {!file && (
+            <PlaceholdersAndVanishInput
+              placeholders={placeholders}
+              onChange={textfieldChangeHandler}
+              onSubmit={textfieldSubmitHandler}
+            />
+          )}
+          {!(file || textVal) && (
+            <div className="p-5 pb-0 text-xl text-center">OR</div>
+          )}
+          {!textVal ? (
+            <>
+              <Link
+                href={""}
+                className="w-full flex gap-1 justify-end pr-2 pb-2"
               >
-                <input {...getInputProps()} />
-                <label
-                  htmlFor="file-upload"
-                  className="flex flex-col items-center justify-center w-full h-64 rounded-[20px] cursor-pointer bg-transparent"
-                >
-                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                    <svg
-                      className="w-10 h-10 mb-3 text-gray-400"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
+                Sample Data <IconInfoCircleFilled />
+              </Link>
+              <form onSubmit={handleSubmit} className="space-y-4">
+                <BackgroundGradient className="rounded-[20px] max-w-md w-full bg-white dark:bg-zinc-900">
+                  <div
+                    {...getRootProps({ className: "dropzone" })}
+                    className="relative flex items-center justify-center w-full rounded-3xl"
+                  >
+                    <input {...getInputProps()} />
+                    <label
+                      htmlFor="file-upload"
+                      className="flex flex-col items-center justify-center w-full h-64 rounded-[20px] cursor-pointer bg-transparent"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-                      ></path>
-                    </svg>
-                    <p className="mb-2 text-sm text-gray-500">
-                      <span className="font-semibold">Click to upload</span> or
-                      drag and drop
-                    </p>
-                    <p className="text-xs text-gray-500">
-                      Excel, PDF, or Image file
-                    </p>
+                      <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                        <svg
+                          className="w-10 h-10 mb-3 text-gray-400"
+                          fill="none"
+                          stroke="currentColor"
+                          viewBox="0 0 24 24"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                          ></path>
+                        </svg>
+                        <p className="mb-2 text-sm text-gray-500">
+                          <span className="font-semibold">Click to upload</span>{" "}
+                          or drag and drop
+                        </p>
+                        <p className="text-xs text-gray-500">
+                          Excel, PDF, or Image file
+                        </p>
+                      </div>
+                    </label>
                   </div>
-                </label>
-              </div>
-            </BackgroundGradient>
+                </BackgroundGradient>
 
-            {/* {file && (
+                {/* {file && (
               <p className="text-sm text-gray-500">
                 Selected file: {file.name}
               </p>
             )} */}
-            <div className="w-full flex justify-between mt-4 items-center px-4">
-              <div className="">
-                {file ? (
-                  <div className="flex  gap-2 w-fit px-4 py-2 items-center bg-[#ff8b31] bg-opacity-20 rounded-[20px] ">
-                    <p>{file?.name}</p>
-                    <button onClick={() => setFile(null)}>
-                      <IconX />
-                    </button>
+                <div className="w-full flex justify-between mt-4 items-center px-4">
+                  <div className="">
+                    {file ? (
+                      <div className="flex  gap-2 w-fit px-4 py-2 items-center bg-[#ff8b31] bg-opacity-20 rounded-[20px] ">
+                        <p>{file?.name}</p>
+                        <button onClick={() => setFile(null)}>
+                          <IconX />
+                        </button>
+                      </div>
+                    ) : (
+                      <p>No Files Selected</p>
+                    )}
                   </div>
-                ) : (
-                  <p>No Files Selected</p>
-                )}
-              </div>
-              <Tooltip
-                className="px-2"
-                arrow={false}
-                content={!file ? "Select a File to Continue" : "Click to Continue"}
-                placement="bottom"
-              >
-                <button
-                  type="submit"
-                  disabled={!file || loading}
-                  className={`w-fit pl-4 pr-2 py-2 text-black dark:text-white rounded-[20px] hover:cursor-pointer ${
-                    !file
-                      ? "bg-gray-500 dark:bg-zinc-900  disabled:cursor-not-allowed"
-                      : loading
-                      ? "bg-gray-500 dark:bg-zinc-900 disabled:cursor-wait"
-                      : "bg-[#ff8b31] hover:bg-[#ff8a31b3]"
-                  }`}
-                >
-                  {loading ? (
-                    <div className="flex gap-1">
-                      Processing
-                      <IconLoaderQuarter className="animate-spin" />
-                    </div>
-                  ) : (
-                    <div className="flex gap-1">
-                      Process
-                      <IconChevronRight />
-                    </div>
-                  )}
-                </button>
-              </Tooltip>
-            </div>
-          </form>
+                  <Tooltip
+                    className="dark:bg-white bg-black text-white dark:text-black px-2"
+                    arrow={false}
+                    content={
+                      !file ? "Select a File to Continue" : "Click to Continue"
+                    }
+                    placement="bottom"
+                  >
+                    <button
+                      type="submit"
+                      disabled={!file || loading}
+                      className={`w-fit pl-4 pr-2 py-2 text-black dark:text-white rounded-[20px] hover:cursor-pointer ${
+                        !file
+                          ? "bg-gray-500 dark:bg-zinc-900  disabled:cursor-not-allowed"
+                          : loading
+                          ? "bg-gray-500 dark:bg-zinc-900 disabled:cursor-wait"
+                          : "bg-[#ff8b31] hover:bg-[#ff8a31b3]"
+                      }`}
+                    >
+                      {loading ? (
+                        <div className="flex gap-1">
+                          Processing
+                          <IconLoaderQuarter className="animate-spin" />
+                        </div>
+                      ) : (
+                        <div className="flex gap-1">
+                          Process
+                          <IconChevronRight />
+                        </div>
+                      )}
+                    </button>
+                  </Tooltip>
+                </div>
+              </form>
+            </>
+          ) : null}
         </>
       )}
       {error && <p className="mt-4 text-red-500">{error}</p>}
